09f094091f55752f3525baec8c01ad95881e1ef1
fORTAKENN <33179005+fORTAKENN@users.noreply.github.com>
2020-12-15 03:22:50 +0200

    PostsUpdate
    postsupdate

---
+++ Services/ItTalks.Services.Data/IPostsService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItTalks.Services.Data
+{
+    public interface IPostsService
+    {
+        public Task<ICollection<PostViewModel>> GetAll(string userId);
+
+        public Task EditPost(EditPostInputModel input);
+      
+
+        public Task<string> AddPost(AddPostmModel input);
+        
+
+        public Task DeletePost(string postId);
+
+        public ICollection<PersonalPostViewModel> GetPersonalPosts(string userId);
+
+        public Task Like(string postId, string userId);
+
+        public EditPostViewModel GetPost(string postId);
+
+        public Task AddPostToUser(string userId, string forumId);
+
+
+    }
+}
---
+++ Services/ItTalks.Services.Data/PostsService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItTalks.Services.Data
+{
+    class PostsService
+    {
+    }
+}
